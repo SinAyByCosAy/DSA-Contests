187de1ec4f0b90bc6646364d241af2f368da7cc6
TanaySrivas <tanaysriofficial@gmail.com>
2023-06-24 01:59:43 +0530

    Sum of ODD length SA done

---
+++ Scaler/SumOfOddLengthSubArrays.java
@@ -0,0 +1,22 @@
+package Contests.Scaler;
+
+import java.util.Scanner;
+
+public class SumOfOddLengthSubArrays {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            int oddSACount = ((i+1) * (n-i) + 1)/2;
+            sum += arr[i] * oddSACount;
+        }
+        System.out.println(sum);
+    }
+}
+//TC -> O(N)
+//SC -> O(1)
